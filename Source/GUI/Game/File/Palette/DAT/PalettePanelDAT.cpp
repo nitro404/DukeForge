2a7905afbf0a20ef4ae83301980d98fdc83d4f13
Kevin Scroggins <nitro404@gmail.com>
2026-08-20 22:26:51 -0400

    Add Build Engine Palette bytes per colour/endianness to palette panel

--- Source/GUI/Game/File/Palette/DAT/PalettePanelDAT.cpp
+++ Source/GUI/Game/File/Palette/DAT/PalettePanelDAT.cpp
@@ -1,5 +1,9 @@
 #include "PalettePanelDAT.h"
 
+#include <Utilities/StringUtilities.h>
+
+#include <magic_enum/magic_enum.hpp>
+
 PalettePanelDAT::PalettePanelDAT(std::unique_ptr<PaletteDAT> palette, wxWindow * parent, wxWindowID windowID, const wxPoint & position, const wxSize & size, long style)
 	: PalettePanel(std::move(palette), parent, windowID, position, size, style)
 	, m_numberOfShadeTablesText(nullptr)
@@ -18,6 +22,18 @@ PalettePanelDAT::PalettePanelDAT(std::unique_ptr<PaletteDAT> palette, wxWindow *
 	numberOfSwapTablesLabel->SetFont(numberOfSwapTablesLabel->GetFont().MakeBold());
 	m_numberOfSwapTablesText = new wxStaticText(numberOfSwapTablesPropertyPanel, wxID_ANY, std::to_string(paletteDAT->numberOfSwapTables()), wxDefaultPosition, wxDefaultSize, wxALIGN_LEFT);
 
+	wxPanel * bytesPerColourPropertyPanel = new wxPanel(m_palettePropertiesPanel, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL);
+
+	wxStaticText * bytesPerColourLabel = new wxStaticText(bytesPerColourPropertyPanel, wxID_ANY, "Bytes Per Colour:", wxDefaultPosition, wxDefaultSize, wxALIGN_RIGHT);
+	bytesPerColourLabel->SetFont(bytesPerColourLabel->GetFont().MakeBold());
+	wxStaticText * bytesPerColourText = new wxStaticText(bytesPerColourPropertyPanel, wxID_ANY, std::to_string(PaletteDAT::BYTES_PER_COLOUR), wxDefaultPosition, wxDefaultSize, wxALIGN_LEFT);
+
+	wxPanel * endiannessPropertyPanel = new wxPanel(m_palettePropertiesPanel, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL);
+
+	wxStaticText * endiannessLabel = new wxStaticText(endiannessPropertyPanel, wxID_ANY, "Endianness:", wxDefaultPosition, wxDefaultSize, wxALIGN_RIGHT);
+	endiannessLabel->SetFont(endiannessLabel->GetFont().MakeBold());
+	wxStaticText * endiannessText = new wxStaticText(endiannessPropertyPanel, wxID_ANY, Utilities::toCapitalCase(magic_enum::enum_name(PaletteDAT::ENDIANNESS)), wxDefaultPosition, wxDefaultSize, wxALIGN_LEFT);
+
 	const int border = 5;
 
 	wxFlexGridSizer * numberOfShadeTablesPropertySizer = new wxFlexGridSizer(2, 3, 3);
@@ -30,8 +46,20 @@ PalettePanelDAT::PalettePanelDAT(std::unique_ptr<PaletteDAT> palette, wxWindow *
 	numberOfSwapTablesPropertySizer->Add(m_numberOfSwapTablesText, 0, wxALIGN_CENTER_VERTICAL, border);
 	numberOfSwapTablesPropertyPanel->SetSizer(numberOfSwapTablesPropertySizer);
 
+	wxFlexGridSizer * endiannessPropertySizer = new wxFlexGridSizer(2, 3, 3);
+	endiannessPropertySizer->Add(endiannessLabel, 0, wxALIGN_CENTER_VERTICAL, border);
+	endiannessPropertySizer->Add(endiannessText, 0, wxALIGN_CENTER_VERTICAL, border);
+	endiannessPropertyPanel->SetSizer(endiannessPropertySizer);
+
+	wxFlexGridSizer * bytesPerColourPropertySizer = new wxFlexGridSizer(2, 3, 3);
+	bytesPerColourPropertySizer->Add(bytesPerColourLabel, 0, wxALIGN_CENTER_VERTICAL, border);
+	bytesPerColourPropertySizer->Add(bytesPerColourText, 0, wxALIGN_CENTER_VERTICAL, border);
+	bytesPerColourPropertyPanel->SetSizer(bytesPerColourPropertySizer);
+
 	m_palettePropertiesSizer->Add(numberOfShadeTablesPropertyPanel, 0, wxLEFT | wxRIGHT, border);
 	m_palettePropertiesSizer->Add(numberOfSwapTablesPropertyPanel, 0, wxLEFT | wxRIGHT, border);
+	m_palettePropertiesSizer->Add(endiannessPropertyPanel, 0, wxLEFT | wxRIGHT, border);
+	m_palettePropertiesSizer->Add(bytesPerColourPropertyPanel, 0, wxLEFT | wxRIGHT, border);
 }
 
 PalettePanelDAT::~PalettePanelDAT() { }
